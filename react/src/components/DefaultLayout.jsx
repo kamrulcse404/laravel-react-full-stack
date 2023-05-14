1ece20ee8e428ece0d5f811cccd01ec6185619d6
kamrulcse404 <kamrul.h01960@gmail.com>
2023-05-14 13:23:06 +0600

    context api

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const DefaultLayout = () => {
+
+    const {user, token} = useStateContext();
+
     return (
         <>
             <Outlet/>    
